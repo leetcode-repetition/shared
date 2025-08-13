1f772f20796af130b3cb6502c5fe42e28cff3bc2
jmurrah <jacobhmurrah@gmail.com>
2025-08-13 12:53:31 -0500

    update get api key

--- database.go
+++ database.go
@@ -48,7 +48,7 @@ func UpsertProblemIntoDatabase(userId string, problem LeetCodeProblem) error {
 	return err
 }
 
-func UpsertApiKeyIntoDatabase(userId string, token string, apiKey string) error {
+func UpsertApiKeyIntoDatabase(userId string, token string, apiKey string, apiKeyCreationTime string) error {
 	if supabaseClient == nil {
 		return fmt.Errorf("supabase client not initialized")
 	}
@@ -56,9 +56,10 @@ func UpsertApiKeyIntoDatabase(userId string, token string, apiKey string) error 
 	table := os.Getenv("SUPABASE_API_TABLE")
 	_, _, err := supabaseClient.From(table).
 		Upsert(map[string]interface{}{
-			"userId": userId,
-			"token":  token,
-			"apiKey": apiKey,
+			"userId":             userId,
+			"token":              token,
+			"apiKey":             apiKey,
+			"apiKeyCreationTime": apiKeyCreationTime,
 		}, "userId,token", "", "").
 		Execute()
 
@@ -128,10 +129,10 @@ func GetProblemsFromDatabase(userId string) []LeetCodeProblem {
 	return problems
 }
 
-func GetApiKeyFromDatabase(userId string, token string) string {
+func GetApiKeyFromDatabase(userId string, token string) (string, string) {
 	if supabaseClient == nil {
 		log.Printf("supabase client not initialized")
-		return ""
+		return "", ""
 	}
 
 	table := os.Getenv("SUPABASE_API_TABLE")
@@ -140,7 +141,7 @@ func GetApiKeyFromDatabase(userId string, token string) string {
 	rawData, _, err := supabaseClient.From(table).Select("*", "", false).Eq("userId", userId).Eq("token", token).Execute()
 	if err != nil {
 		log.Printf("Error fetching data: %v", err)
-		return ""
+		return "", ""
 	}
 
 	log.Printf("Raw data: %s", string(rawData))
@@ -149,19 +150,20 @@ func GetApiKeyFromDatabase(userId string, token string) string {
 	err = json.Unmarshal(rawData, &results)
 	if err != nil {
 		log.Printf("Error unmarshaling data: %v", err)
-		return ""
+		return "", ""
 	}
 
 	if len(results) == 0 {
-		return ""
+		return "", ""
 	}
 
-	apiKey, ok := results[0]["apiKey"].(string)
-	if !ok {
-		return ""
+	apiKey, okApiKey := results[0]["apiKey"].(string)
+	apiKeyCreationTime, okApiKeyCreationTime := results[0]["apiKeyCreationTime"].(string)
+	if !okApiKey || !okApiKeyCreationTime {
+		return "", ""
 	}
 
-	return apiKey
+	return apiKey, apiKeyCreationTime
 }
 
 func DeleteAllProblemsFromDatabase(userId string) error {
